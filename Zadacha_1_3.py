469d45086a8c0b5a81c8e052c966eb14dc392c88
SvySor <svysor@gmail.com>
2023-02-02 21:31:36 +0300

    Zadacha_1_3 solved

--- Zadacha_1_3.py
+++ Zadacha_1_3.py
@@ -6,45 +6,19 @@
 
 
 correctInput = False
-while correctInput == True:
+while correctInput == False:
     pointX = float(input("Input X coordinate: "))
-    pointY = float(input("Input X coordinate: "))
-    if (pointX or pointY) != 0:
+    pointY = float(input("Input Y coordinate: "))
+    if pointX == 0 or pointY == 0:
+        print("Enter ones more time correct coordinates")
+    else:
         correctInput = True
-else:
-    print("Enter correct coordinates")
-
-
-
-
-
-# def inputKoord(x):
-#     a = [0] * x
-#     for i in range(x):
-#         is_OK = False
-#         while not is_OK:
-#             try:
-#                 number = float(input(f"Введите {i+1} координату: "))
-#                 a[i] = number
-#                 is_OK = True
-#                 if a[i] == 0:
-#                     is_OK = False
-#                     print("Координата не должно быть равна 0 ")
-#             except ValueError:
-#                 print("Ты ошибся. Вводить надо числа!")
-#     return a
-
-
-# def checkCoordinates(xy):
-#     text = 4
-#     if xy[0] > 0 and xy[1] > 0:
-#         text = 1
-#     elif xy[0] < 0 and xy[1] > 0:
-#         text = 2
-#     elif xy[0] < 0 and xy[1] < 0:
-#         text = 3
-#     print(f"Точка находится в {text} четверти плоскости")
-
-
-# koordinate = inputKoord(2)
-# checkCoordinates(koordinate)
+print(f"Great, your point is: {pointX}, {pointY}")
+quarterNumber = 4
+if pointX > 0 and pointY > 0:
+    quarterNumber = 1
+elif pointX > 0 and pointY < 0:
+    quarterNumber = 2
+elif pointX < 0 and pointY < 0:
+    quarterNumber = 3
+print(f"Your point locate in {quarterNumber} quarter")
